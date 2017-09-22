cfa6c0caa624ad0cb5beff3b3eba7ac41833e988
Nicola Del Gobbo <nicoladelgobbo@gmail.com>
2017-09-22 23:30:49 +0200

    Final example

---
+++ vedis/src/kvdb.h
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) Nicola Del Gobbo
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the license at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+ * IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+ * MERCHANTABLITY OR NON-INFRINGEMENT.
+ *
+ * See the Apache Version 2.0 License for specific language governing
+ * permissions and limitations under the License.
+ *
+ * Contributors - initial API implementation:
+ * Nicola Del Gobbo <nicoladelgobbo@gmail.com>
+ ******************************************************************************/
+
+#ifndef KVDB_H
+#define KVDB_H
+
+#include <nan.h>
+#include "deps/vedis.h"
+
+
+using namespace v8;
+
+namespace KVDB {
+
+    class Database : public Nan::ObjectWrap {
+        public: 
+            static NAN_MODULE_INIT(Init);
+            static NAN_METHOD(New);
+            static NAN_METHOD(GetKey);
+            static NAN_METHOD(GetKeyBuffer);
+            static NAN_METHOD(GetKeySync);
+            static NAN_METHOD(PutKey);
+            static NAN_METHOD(PutKeyBuffer);
+            static NAN_METHOD(PutKeySync);
+            static NAN_GETTER(DbName);
+        private:
+            Database(std::string db_name);
+            ~Database();
+            std::string db_name;
+            vedis *db;
+            static inline Persistent<v8::Function> & constructor();
+    };
+
+    static std::string root_path = "./tmp";
+    static std::string db_extension = ".db";
+
+}
+
+#endif //KVDB_H
